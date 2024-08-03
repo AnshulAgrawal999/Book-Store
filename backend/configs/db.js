ee721ae166489c094b5d27390c69306272402381
anshul <anshulagr799@gmail.com>
2024-08-03 22:28:54 +0530

    db setup

---
+++ backend/configs/db.js
@@ -0,0 +1,11 @@
+const mongoose = require( 'mongoose' )  ;
+
+const dotenv = require( 'dotenv' )  ;
+
+dotenv.config() ;
+
+
+const connection = mongoose.connect( process.env.mongoURL )  ;
+
+
+module.exports = { connection }  ;
--- backend/index.js
+++ backend/index.js
@@ -3,3 +3,36 @@ const express = require( 'express' )  ;
 const dotenv = require( 'dotenv' )  ;
 
 dotenv.config()  ;
+
+const cors = require( 'cors' )  ;
+
+const { connection } = require( './configs/db' )  ;
+
+const PORT = process.env.PORT || 6000  ;
+
+const app = express()  ;
+
+app.use( cors() )  ;
+
+app.use( express.json() )  ;
+
+app.get( '/' , ( req , res ) => {
+
+    res.send( { 'msg' : 'this is Book-Store home page' } )  ;
+    
+} )  ;
+
+
+app.listen( PORT , async ()=>{
+    try {
+        console.log( `server is running on http://localhost:${PORT}` )  ;
+        
+        await connection  ;
+
+        console.log( 'server is connected to DB' )  ;
+
+    } catch (error) {
+        
+        console.log( { error } )  ;
+    }
+} )  ;
